8721a295d8b508a8bbfd2a22ba327a3fdc01b376
Jesse Mendez <jessepmendez79@gmail.com>
2025-03-16 15:31:35 -0500

    Added treesitter grammars

--- emacs/dot-emacs.d/config.el
+++ emacs/dot-emacs.d/config.el
@@ -223,6 +223,33 @@
 (setq org-crypt-key "jessepmendez79@gmail.com")
 (setq epa-pinentry-mode 'loopback)
 
+;; Tree-sitter
+  (setq treesit-language-source-alist
+   '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+     (c . ("https://github.com/tree-sitter/tree-sitter-c.git"))
+     (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp.git"))
+     (cmake . ("https://github.com/uyha/tree-sitter-cmake"))
+     (css . ("https://github.com/tree-sitter/tree-sitter-css"))
+     (csv . ("https://github.com/tree-sitter-grammars/tree-sitter-csv" "master" "csv/src"))
+     (elisp . ("https://github.com/Wilfred/tree-sitter-elisp"))
+     (gpg-config . ("https://github.com/tree-sitter-grammars/tree-sitter-gpg-config.git"))
+     (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+     (go . ("https://github.com/tree-sitter/tree-sitter-go"))
+     (html . ("https://github.com/tree-sitter/tree-sitter-html ppp"))
+     (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+     (json . ("https://github.com/tree-sitter/tree-sitter-json"))
+     (lua . ("https://github.com/tree-sitter-grammars/tree-sitter-lua.git"))
+     (make . ("https://github.com/alemuller/tree-sitter-make"))
+     (markdown . ("https://github.com/ikatyang/tree-sitter-markdown"))
+     (properties . ("https://github.com/tree-sitter-grammars/tree-sitter-properties.git"))
+     (ssh-config . ("https://github.com/tree-sitter-grammars/tree-sitter-ssh-config.git"))
+     (toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
+     (udev . ("https://github.com/tree-sitter-grammars/tree-sitter-udev.git"))
+     (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+     (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+     (yaml . ("https://github.com/ikatyang/tree-sitter-yaml"))
+     (xml . ("https://github.com/tree-sitter-grammars/tree-sitter-xml.git"))))
+
 
 (use-package magit
   :straight t

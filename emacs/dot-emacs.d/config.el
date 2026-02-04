897e2dc3ef3f39cf4a4c97a75241cf4c3668aa96
Jesse Mendez <jessepmendez79@gmail.com>
2026-02-04 10:22:03 -0600

    Updates

--- emacs/dot-emacs.d/config.el
+++ emacs/dot-emacs.d/config.el
@@ -25,6 +25,141 @@
 
 (require 'plstore)
 
+
+    ;;; Fira code
+    ;; This works when using emacs --daemon + emacsclient
+    (add-hook 'after-make-frame-functions (lambda (frame) (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol")))
+    ;; This works when using emacs without server/client
+    (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol")
+    ;; I haven't found one statement that makes both of the above situations work, so I use both for now
+     
+    (defconst fira-code-font-lock-keywords-alist
+      (mapcar (lambda (regex-char-pair)
+                `(,(car regex-char-pair)
+                  (0 (prog1 ()
+                       (compose-region (match-beginning 1)
+                                       (match-end 1)
+                                       ;; The first argument to concat is a string containing a literal tab
+                                       ,(concat "	" (list (decode-char 'ucs (cadr regex-char-pair)))))))))
+              '(("\\(www\\)"                   #Xe100)
+                ("[^/]\\(\\*\\*\\)[^/]"        #Xe101)
+                ("\\(\\*\\*\\*\\)"             #Xe102)
+                ("\\(\\*\\*/\\)"               #Xe103)
+                ("\\(\\*>\\)"                  #Xe104)
+                ("[^*]\\(\\*/\\)"              #Xe105)
+                ("\\(\\\\\\\\\\)"              #Xe106)
+                ("\\(\\\\\\\\\\\\\\)"          #Xe107)
+                ("\\({-\\)"                    #Xe108)
+                ("\\(\\[\\]\\)"                #Xe109)
+                ("\\(::\\)"                    #Xe10a)
+                ("\\(:::\\)"                   #Xe10b)
+                ("[^=]\\(:=\\)"                #Xe10c)
+                ("\\(!!\\)"                    #Xe10d)
+                ("\\(!=\\)"                    #Xe10e)
+                ("\\(!==\\)"                   #Xe10f)
+                ("\\(-}\\)"                    #Xe110)
+                ("\\(--\\)"                    #Xe111)
+                ("\\(---\\)"                   #Xe112)
+                ("\\(-->\\)"                   #Xe113)
+                ("[^-]\\(->\\)"                #Xe114)
+                ("\\(->>\\)"                   #Xe115)
+                ("\\(-<\\)"                    #Xe116)
+                ("\\(-<<\\)"                   #Xe117)
+                ("\\(-~\\)"                    #Xe118)
+                ("\\(#{\\)"                    #Xe119)
+                ("\\(#\\[\\)"                  #Xe11a)
+                ("\\(##\\)"                    #Xe11b)
+                ("\\(###\\)"                   #Xe11c)
+                ("\\(####\\)"                  #Xe11d)
+                ("\\(#(\\)"                    #Xe11e)
+                ("\\(#\\?\\)"                  #Xe11f)
+                ("\\(#_\\)"                    #Xe120)
+                ("\\(#_(\\)"                   #Xe121)
+                ("\\(\\.-\\)"                  #Xe122)
+                ("\\(\\.=\\)"                  #Xe123)
+                ("\\(\\.\\.\\)"                #Xe124)
+                ("\\(\\.\\.<\\)"               #Xe125)
+                ("\\(\\.\\.\\.\\)"             #Xe126)
+                ("\\(\\?=\\)"                  #Xe127)
+                ("\\(\\?\\?\\)"                #Xe128)
+                ("\\(;;\\)"                    #Xe129)
+                ("\\(/\\*\\)"                  #Xe12a)
+                ("\\(/\\*\\*\\)"               #Xe12b)
+                ("\\(/=\\)"                    #Xe12c)
+                ("\\(/==\\)"                   #Xe12d)
+                ("\\(/>\\)"                    #Xe12e)
+                ("\\(//\\)"                    #Xe12f)
+                ("\\(///\\)"                   #Xe130)
+                ("\\(&&\\)"                    #Xe131)
+                ("\\(||\\)"                    #Xe132)
+                ("\\(||=\\)"                   #Xe133)
+                ("[^|]\\(|=\\)"                #Xe134)
+                ("\\(|>\\)"                    #Xe135)
+                ("\\(\\^=\\)"                  #Xe136)
+                ("\\(\\$>\\)"                  #Xe137)
+                ("\\(\\+\\+\\)"                #Xe138)
+                ("\\(\\+\\+\\+\\)"             #Xe139)
+                ("\\(\\+>\\)"                  #Xe13a)
+                ("\\(=:=\\)"                   #Xe13b)
+                ("[^!/]\\(==\\)[^>]"           #Xe13c)
+                ("\\(===\\)"                   #Xe13d)
+                ("\\(==>\\)"                   #Xe13e)
+                ("[^=]\\(=>\\)"                #Xe13f)
+                ("\\(=>>\\)"                   #Xe140)
+                ("\\(<=\\)"                    #Xe141)
+                ("\\(=<<\\)"                   #Xe142)
+                ("\\(=/=\\)"                   #Xe143)
+                ("\\(>-\\)"                    #Xe144)
+                ("\\(>=\\)"                    #Xe145)
+                ("\\(>=>\\)"                   #Xe146)
+                ("[^-=]\\(>>\\)"               #Xe147)
+                ("\\(>>-\\)"                   #Xe148)
+                ("\\(>>=\\)"                   #Xe149)
+                ("\\(>>>\\)"                   #Xe14a)
+                ("\\(<\\*\\)"                  #Xe14b)
+                ("\\(<\\*>\\)"                 #Xe14c)
+                ("\\(<|\\)"                    #Xe14d)
+                ("\\(<|>\\)"                   #Xe14e)
+                ("\\(<\\$\\)"                  #Xe14f)
+                ("\\(<\\$>\\)"                 #Xe150)
+                ("\\(<!--\\)"                  #Xe151)
+                ("\\(<-\\)"                    #Xe152)
+                ("\\(<--\\)"                   #Xe153)
+                ("\\(<->\\)"                   #Xe154)
+                ("\\(<\\+\\)"                  #Xe155)
+                ("\\(<\\+>\\)"                 #Xe156)
+                ("\\(<=\\)"                    #Xe157)
+                ("\\(<==\\)"                   #Xe158)
+                ("\\(<=>\\)"                   #Xe159)
+                ("\\(<=<\\)"                   #Xe15a)
+                ("\\(<>\\)"                    #Xe15b)
+                ("[^-=]\\(<<\\)"               #Xe15c)
+                ("\\(<<-\\)"                   #Xe15d)
+                ("\\(<<=\\)"                   #Xe15e)
+                ("\\(<<<\\)"                   #Xe15f)
+                ("\\(<~\\)"                    #Xe160)
+                ("\\(<~~\\)"                   #Xe161)
+                ("\\(</\\)"                    #Xe162)
+                ("\\(</>\\)"                   #Xe163)
+                ("\\(~@\\)"                    #Xe164)
+                ("\\(~-\\)"                    #Xe165)
+                ("\\(~=\\)"                    #Xe166)
+                ("\\(~>\\)"                    #Xe167)
+                ("[^<]\\(~~\\)"                #Xe168)
+                ("\\(~~>\\)"                   #Xe169)
+                ("\\(%%\\)"                    #Xe16a)
+                ;;("\\(x\\)"                     #Xe16b)
+                ("[^:=]\\(:\\)[^:=]"           #Xe16c)
+                ("[^\\+<>]\\(\\+\\)[^\\+<>]"   #Xe16d)
+                ("[^\\*/<>]\\(\\*\\)[^\\*/<>]" #Xe16f))))
+     
+    (defun add-fira-code-symbol-keywords ()
+      (font-lock-add-keywords nil fira-code-font-lock-keywords-alist))
+     
+    (add-hook 'prog-mode-hook
+              #'add-fira-code-symbol-keywords)
+
+
 ;; Auth-source pass
 (use-package pass
   :straight t
@@ -54,13 +189,13 @@
   (flyspell-mode 1)
   ;; (org-fragtog-mode 1)
   (visual-line-mode 1)
-  (fira-code-mode 1)
+  ;; (fira-code-mode 1)
   )
 (defun my-ledger-hook ()
   (setq-local tab-always-indent 'complete)
   (setq-local completion-cycle-threshold t)
   (setq-local ledger-complete-in-steps t)
-  (fira-code-mode 1)
+  ;; (fira-code-mode 1)
   )
 
 (defun my-denote-mode-hook ()
@@ -173,14 +308,14 @@ and assumes the default Org-roam naming scheme."
 (column-number-mode 1)
 ;; (windmove-default-keybindings)
 
-(use-package fira-code-mode
-  :straight t
-  :config
-  (fira-code-mode-set-font)
-  :hook prog-mode
-  ;; (global-fira-code-mode)
-  :custom
-  (fira-code-mode-disabled-ligatures '("[]" "#{" "#(" "#_" "#_(" "x")))
+;; (use-package fira-code-mode
+;;   :straight t
+;;   :config
+;;   (fira-code-mode-set-font)
+;;   :hook prog-mode
+;;   ;; (global-fira-code-mode)
+;;   :custom
+;;   (fira-code-mode-disabled-ligatures '("[]" "#{" "#(" "#_" "#_(" "x")))
   
 ;; Save File
 (setq delete-old-versions t)
@@ -588,7 +723,7 @@ and assumes the default Org-roam naming scheme."
 (use-package org-roam
   :straight t
   :bind
-  (("C-c r d" . org-roam-dailies-capture-today)
+  (("C-c r j" . org-roam-dailies-capture-today)
    ("C-c r f" . org-roam-node-find)
    ("C-c r i" . org-roam-node-insert))
   :config
@@ -604,6 +739,10 @@ and assumes the default Org-roam naming scheme."
       (error "")))
   (setq org-roam-directory "~/Sync/org/roam")
   (setq org-roam-dailies-directory "journals/")
+  (setq org-roam-dailies-capture-templates '(("j" "journal"
+                                              entry
+                                              "* %?"
+                                              :target (file+head "%<%Y%m%d>.org" "#+title: %<%Y-%m-%d>\n"))))
   (setq org-roam-capture-templates '(("d" "default"
                                       plain
                                       "%?"
@@ -623,12 +762,10 @@ and assumes the default Org-roam naming scheme."
 				      (file "~/Sync/org/roam/ref/notes-template.org")
 				      :target (file+head "ref/${citekey}.org"
 							 "#+title: ${title}\n")
-				      :empty-lines 1)))
+				      :empty-lines 1))
 
-  (setq org-roam-dailies-capture-templates '(("d" "default"
-                                              entry
-                                              "* %?"
-                                              :target (file+head "%<%Y%m%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+)
+
   (setq org-roam-node-display-template
 	(concat "${type:15} ${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   #("${type:15} ${title:*} ${tags:10}" 22 32 (face org-tag))
